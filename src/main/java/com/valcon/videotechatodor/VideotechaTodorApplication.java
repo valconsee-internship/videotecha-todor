c767043eee37f83a67c3a4cbc6b7467a2f5d586b
Todor Popovic <todorpopovic@Todors-MacBook-Pro.local>
2023-05-05 14:17:28 +0200

    Initializing project

---
+++ src/main/java/com/valcon/videotechatodor/VideotechaTodorApplication.java
@@ -0,0 +1,13 @@
+package com.valcon.videotechatodor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideotechaTodorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideotechaTodorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/valcon/videotechatodor/VideotechaTodorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.valcon.videotechatodor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideotechaTodorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
