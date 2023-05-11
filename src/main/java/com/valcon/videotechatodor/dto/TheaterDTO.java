0556ff49327bb55ad164d5bf13f934b5ebb9851a
Todor Popovic <todorpopovic@Todors-MacBook-Pro.local>
2023-05-11 11:20:00 +0200

    VAL-2 Added endpoints for getting theater(s)

---
+++ src/main/java/com/valcon/videotechatodor/dto/TheaterDTO.java
@@ -0,0 +1,26 @@
+package com.valcon.videotechatodor.dto;
+
+import com.valcon.videotechatodor.utils.RoomType;
+
+import java.io.Serializable;
+
+public class TheaterDTO implements Serializable {
+
+    private final RoomType roomType;
+
+    private final int capacity;
+
+    public TheaterDTO(RoomType roomType, int capacity) {
+        this.roomType = roomType;
+        this.capacity = capacity;
+    }
+
+    public RoomType getRoomType() {
+        return roomType;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+}
---
+++ src/main/java/com/valcon/videotechatodor/service/impl/TheaterServiceImpl.java
@@ -0,0 +1,39 @@
+package com.valcon.videotechatodor.service.impl;
+
+import com.valcon.videotechatodor.dto.TheaterDTO;
+import com.valcon.videotechatodor.mapper.TheaterMapper;
+import com.valcon.videotechatodor.model.Theater;
+import com.valcon.videotechatodor.repository.TheaterRepository;
+import com.valcon.videotechatodor.service.TheaterService;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class TheaterServiceImpl implements TheaterService {
+
+    private final TheaterRepository theaterRepository;
+
+    public TheaterServiceImpl(TheaterRepository theaterRepository) {
+        this.theaterRepository = theaterRepository;
+    }
+
+    @Override
+    public TheaterDTO getOne(Long id) {
+        return TheaterMapper.toDTO(theaterRepository.findById(id).orElseThrow(
+                () -> new RuntimeException("Theater with ID " + id + " does not exits"))
+        );
+    }
+
+    @Override
+    public List<TheaterDTO> getAll() {
+        List<Theater> theaters = theaterRepository.findAll();
+        List<TheaterDTO> theaterDTOS = new ArrayList<>();
+        for(Theater theater: theaters){
+            theaterDTOS.add(TheaterMapper.toDTO(theater));
+        }
+        return theaterDTOS;
+    }
+
+}
